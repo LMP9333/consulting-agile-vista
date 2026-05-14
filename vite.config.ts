7c0f3e5032fcc0b9d47c4bb54b2d1484d50f2f65
LMP9333 <lemichepetersen@icloud.com>
2026-05-14 11:10:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/consulting-agile-vista/" : "/",
+  base: "/consulting-agile-vista/",
   server: {
     host: "::",
     port: 8080,
